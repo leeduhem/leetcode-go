4057817ac3dd44c0cc19653d6a67e81eb0ec3b08
Lee Duhem <lee.duhem@gmail.com>
2017-10-03 12:17:32 +0800

    Solution to Problem #3

---
+++ src/p003/p003.go
@@ -0,0 +1,33 @@
+package p003
+
+func lengthOfLongestSubstring(s string) int {
+	max := 0
+	if (len(s) > 0) {
+		max = 1
+	}
+
+	for i,vi := range s {
+		m := make(map[rune]int, 26)
+		m[vi] = i
+
+		for j,vj := range s[i+1:] {
+			_,found := m[vj]
+			if found {
+				break
+			} else {
+				m[vj] = j + i + 1
+			}
+
+			// (j + i + 1) - i + 1 = j + 2
+			if (j + 2 > max) {
+				max = j + 2
+			}
+		}
+	}
+
+	return max
+}
+
+func LengthOfLongestSubstring(s string) int {
+	return lengthOfLongestSubstring(s)
+}
---
+++ src/p003/p003_test.go
@@ -0,0 +1,15 @@
+package p003
+
+import "testing"
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	ss := []string { "", "c", "bbbbb", "au", "abcabcbb", "pwwkew" }
+	rs := []int    {  0,  1,   1,       2,    3,          3       }
+	for i,s := range ss {
+		max := lengthOfLongestSubstring(s)
+		if (max != rs[i]) {
+			t.Errorf("Expected %c", rs[i])
+		}
+	}
+}
+
