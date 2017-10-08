ab383356b4b82479fd21764eb8c6056026f3737a
Lee Duhem <lee.duhem@gmail.com>
2017-10-08 19:33:21 +0800

    Solution to Problem #30

---
+++ src/p030/p030.go
@@ -0,0 +1,47 @@
+package p030
+
+func findSubstring(s string, words []string) []int {
+	k := len(words)
+	inds := []int {}
+
+	if k == 0 {
+		return inds
+	}
+
+	wlen := len(words[0])
+	slen := len(s)
+	if k * wlen > slen {
+		return inds
+	}
+
+	for i := 0; i <= slen - k * wlen; i++ {
+		m := make(map[string]int, k)
+
+		for j := i; j < i + k * wlen; j += wlen {
+			key := s[j:j+wlen]
+			if v,ok := m[key]; ok {
+				m[key] = v + 1
+			} else {
+				m[key] = 1
+			}
+		}
+
+		for _,w := range words {
+			if v,ok := m[w]; ok {
+				if v == 1 {
+					delete(m, w)
+				} else {
+					m[w] = v - 1
+				}
+			} else {
+				break
+			}
+		}
+
+		if len(m) == 0 {
+			inds = append(inds, i)
+		}
+	}
+
+	return inds
+}
