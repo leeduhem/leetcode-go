c81872b4228e691ad1ba11485b63637634702285
Lee Duhem <lee.duhem@gmail.com>
2018-04-16 23:24:22 +0800

    Revise Solution to Problem #1

--- src/p001/p001.go
+++ src/p001/p001.go
@@ -1,10 +1,11 @@
+// https://leetcode.com/problems/two-sum/description/
 package p001
 
 func twoSum(nums []int, target int) []int {
-	for i,vi := range nums {
-		for j,vj := range (nums[i+1:]) {
-			if (vi + vj == target) {
-				return []int {i,j+i+1}
+	for i, vi := range nums {
+		for j, vj := range nums[i+1:] {
+			if vi+vj == target {
+				return []int{i, j + i + 1}
 			}
 		}
 	}
