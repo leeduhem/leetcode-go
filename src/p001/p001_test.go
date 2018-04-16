20967e33fc914776797ce860bfc7beea9f1d4720
Lee Duhem <lee.duhem@gmail.com>
2018-04-16 23:24:22 +0800

    Revise Solution of Problem #1

--- src/p001/p001_test.go
+++ src/p001/p001_test.go
@@ -1,19 +1,17 @@
 package p001
 
-import "testing"
+import (
+	"testing"
+	. "testing/utils"
+)
 
 func TestTwoSum(t *testing.T) {
-	nums1 := []int {3, 2, 4}
-	target1 := 6
-	answer1 := []int {1, 2}
-
-	indins1 := twoSum(nums1, target1)
-	if (len(indins1) != len(answer1)) {
-		t.Fatalf("Case %v: expected %v, got %v", nums1, answer1, indins1)
-	}
-	for i,v := range indins1 {
-		if (v != answer1[i]) {
-			t.Fatalf("Case %v: expected %v, got %v", nums1, answer1, indins1)
+	testCase := func(in0 []int, in1 int, want []int) {
+		got := twoSum(in0, in1)
+		if !IsIntsEqual(got, want) {
+			t.Errorf("Case %v, %v: expected %v, got %v",
+				in0, in1, want, got)
 		}
 	}
+	testCase([]int{3, 2, 4}, 6, []int{1, 2})
 }
