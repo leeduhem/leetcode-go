f31713c201908cdee940bf40a3bad72d4f2fdbf0
Lee Duhem <lee.duhem@gmail.com>
2018-04-18 23:39:23 +0800

    Revise Another Solution to Problem #2

---
+++ src/p002_1/ListUtils.go
@@ -0,0 +1,62 @@
+package p002_1
+
+import (
+	"fmt"
+	"strings"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func (l *ListNode) String() string {
+	var (
+		builder strings.Builder
+		cnt     int
+	)
+
+	for n := l; n != nil; n = n.Next {
+		fmt.Fprintf(&builder, "&{%v -> ", n.Val)
+		cnt++
+	}
+	fmt.Fprint(&builder, "<nil>", strings.Repeat("}", cnt))
+
+	return builder.String()
+}
+
+func isListEqual(l1, l2 *ListNode) bool {
+	for ; l1 != nil && l2 != nil; l1, l2 = l1.Next, l2.Next {
+		if l1.Val != l2.Val {
+			return false
+		}
+	}
+
+	if l1 != nil || l2 != nil {
+		return false
+	}
+
+	return true
+}
+
+func deepCopyList(l *ListNode) *ListNode {
+	l1 := (*ListNode)(nil)
+
+	for tail := &l1; l != nil; l = l.Next {
+		*tail = &ListNode{l.Val, nil}
+		tail = &(*tail).Next
+	}
+
+	return l1
+}
+
+func ints2List(ns []int) *ListNode {
+	head := (*ListNode)(nil)
+	tail := &head
+
+	for _, n := range ns {
+		*tail = &ListNode{n, nil}
+		tail = &(*tail).Next
+	}
+	return head
+}
