f6152ca1a6e84e07e360f60b502f3ed75b9ec0be
Lee Duhem <lee.duhem@gmail.com>
2018-04-18 23:39:23 +0800

    Revise Another Solution to Problem #2

--- src/p002_1/p002_1.go
+++ src/p002_1/p002_1.go
@@ -1,10 +1,5 @@
 package p002_1
 
-type ListNode struct {
-	Val int
-	Next *ListNode
-}
-
 func number2Digit(n *ListNode) <-chan int {
 	d := make(chan int)
 	go func() {
@@ -18,18 +13,20 @@ func number2Digit(n *ListNode) <-chan int {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	sum := &ListNode{ 0, nil }
-	end := sum
+	sum := &ListNode{0, nil}
 	carry := 0
 
 	ds1 := number2Digit(l1)
 	ds2 := number2Digit(l2)
 
-	for {
-		d1,ok1 := <-ds1
-		d2,ok2 := <-ds2
+	for tail := sum; ; tail = tail.Next {
+		d1, ok1 := <-ds1
+		d2, ok2 := <-ds2
 
 		if !ok1 && !ok2 {
+			if carry != 0 {
+				tail.Next = &ListNode{carry, nil}
+			}
 			break
 		}
 
@@ -41,13 +38,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			carry = 0
 		}
 
-		end.Next = &ListNode{ s, nil }
-		end = end.Next
-	}
-
-	if carry != 0 {
-		end.Next = &ListNode{ carry, nil }
-		end = end.Next
+		tail.Next = &ListNode{s, nil}
 	}
 
 	return sum.Next
