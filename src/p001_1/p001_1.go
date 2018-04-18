0bb7daf165fa959c25f5377365e7f2418ec9f53e
Lee Duhem <lee.duhem@gmail.com>
2018-04-18 23:26:52 +0800

    Revise Another Solution to Problem #1

--- src/p001_1/p001_1.go
+++ src/p001_1/p001_1.go
@@ -1,15 +1,14 @@
+// https://leetcode.com/problems/two-sum/description/
 package p001_1
 
 func twoSum(nums []int, target int) []int {
 	var m = make(map[int]int, len(nums))
 
-	for i,v := range nums {
-		var res = target - v
-		var j,found = m[res]
-		if found {
-			return []int {j, i}
+	for i, v := range nums {
+		res := target - v
+		if j, ok := m[res]; ok {
+			return []int{j, i}
 		}
-
 		m[v] = i
 	}
 
