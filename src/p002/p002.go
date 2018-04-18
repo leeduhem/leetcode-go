bcf54bc8cfc4f3a960b9e1da6c919a9713d861f6
Lee Duhem <lee.duhem@gmail.com>
2018-04-18 23:18:51 +0800

    Revise Solution of Problem #2

--- src/p002/p002.go
+++ src/p002/p002.go
@@ -1,21 +1,14 @@
 package p002
 
-type ListNode struct {
-	Val int
-	Next *ListNode
-}
-
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	sum := &ListNode{ 0, nil }
-	end := sum
+	sum := &ListNode{0, nil}
 	carry := 0
 
-	for {
+	for tail := sum; ; tail = tail.Next {
 		s := carry
 		if l1 == nil && l2 == nil {
 			if carry != 0 {
-				end.Next = &ListNode { 1, nil }
-				end = end.Next
+				tail.Next = &ListNode{1, nil}
 			}
 			break
 		}
@@ -36,8 +29,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			carry = 0
 		}
 
-		end.Next = &ListNode{ s, nil }
-		end = end.Next
+		tail.Next = &ListNode{s, nil}
 	}
 
 	return sum.Next
--- src/p002/p002_test.go
+++ src/p002/p002_test.go
@@ -1,29 +1,24 @@
 package p002
 
-import "testing"
+import (
+	"testing"
+)
 
-func testCase(a1,a2,s0 *ListNode, t *testing.T) {
-	s1 := addTwoNumbers(a1, a2)
-
-	for ; s0 != nil && s1 != nil; s0,s1 = s0.Next, s1.Next {
-		if (s0.Val != s1.Val) {
-			t.Fatalf("Case %v %v: expected %v, got %v", a1, a2, s0, s1)
+func TestAddTwoNumbers(t *testing.T) {
+	testCase := func(in0, in1, want *ListNode) {
+		got := addTwoNumbers(in0, in1)
+		if !isListEqual(got, want) {
+			t.Errorf("Case %v, %v: expected %v, got %v",
+				in0, in1, want, got)
 		}
 	}
 
-	if s0 != nil || s1 != nil {
-		t.Fatalf("Case %v %v: expected %v, got %v", a1, a2, s0, s1)
-	}
-}
+	in00 := ints2List([]int{2, 4, 3})
+	in01 := ints2List([]int{5, 6, 4})
+	want0 := ints2List([]int{7, 0, 8})
+	testCase(in00, in01, want0)
 
-func TestAddTwoNumbers(t *testing.T) {
-	l11 := &ListNode{2, &ListNode{4, &ListNode{3, nil}}}
-	l12 := &ListNode{5, &ListNode{6, &ListNode{4, nil}}}
-	r10 := &ListNode{7, &ListNode{0, &ListNode{8, nil}}}
-	testCase(l11, l12, r10, t)
-
-
-	l21 := &ListNode{5, nil}
-	r20 := &ListNode{0, &ListNode{1, nil}}
-	testCase(l21, l21, r20, t)
+	in10 := ints2List([]int{5})
+	want1 := ints2List([]int{0, 1})
+	testCase(in10, in10, want1)
 }
