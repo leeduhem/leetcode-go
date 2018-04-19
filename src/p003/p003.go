e2cdae4eed2a6a56bb49ce4aecca418c850dad63
Lee Duhem <lee.duhem@gmail.com>
2018-04-19 23:00:56 +0800

    Revise Solution to Problem #3

--- src/p003/p003.go
+++ src/p003/p003.go
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 package p003
 
 func lengthOfLongestSubstring(s string) int {
@@ -6,20 +7,19 @@ func lengthOfLongestSubstring(s string) int {
 		max = 1
 	}
 
-	for i,vi := range s {
+	for i, vi := range s {
 		m := make(map[rune]int, 26)
 		m[vi] = i
 
-		for j,vj := range s[i+1:] {
-			_,found := m[vj]
-			if found {
+		for j, vj := range s[i+1:] {
+			if _, ok := m[vj]; ok {
 				break
 			} else {
 				m[vj] = j + i + 1
 			}
 
 			// (j + i + 1) - i + 1 = j + 2
-			if j + 2 > max {
+			if j+2 > max {
 				max = j + 2
 			}
 		}
