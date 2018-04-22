83ee62a88b512578ad4e17c41b1480dac7074d78
Lee Duhem <lee.duhem@gmail.com>
2018-04-22 23:40:04 +0800

    Revise Solution to Problem #4

--- src/p004/p004.go
+++ src/p004/p004.go
@@ -1,42 +1,40 @@
+// https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 package p004
 
-func min(a, b int) int {
-	if (a < b) {
-		return a
-	} else {
-		return b
+func intMin(x, y int) int {
+	if x < y {
+		return x
 	}
+	return y
 }
 
-func getkth(s, l []int, k int) int {
-	m,n := len(s), len(l)
+func getKth(s, l []int, k int) int {
+	m, n := len(s), len(l)
 
 	// let m <= n
-	if (m > n) {
-		return getkth(l, s, k)
+	if m > n {
+		return getKth(l, s, k)
 	}
 
-	if (m == 0) {
+	if m == 0 {
 		return l[k-1]
 	}
-	if (k == 1) {
-		return min(s[0], l[0])
+	if k == 1 {
+		return intMin(s[0], l[0])
 	}
 
-	i := min(m, k/2)
-	j := min(n, k/2)
-	if (s[i-1] > l[j-1]) {
-		return getkth(s, l[j:], k-j)
-	} else {
-		return getkth(s[i:], l, k-i)
+	i := intMin(m, k/2)
+	j := intMin(n, k/2)
+	if s[i-1] > l[j-1] {
+		return getKth(s, l[j:], k-j)
 	}
-	return 0
+	return getKth(s[i:], l, k-i)
 }
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	m,n := len(nums1), len(nums2)
+	m, n := len(nums1), len(nums2)
 	l := (m + n + 1) / 2
 	r := (m + n + 2) / 2
 
-	return (float64) (getkth(nums1, nums2, l) + getkth(nums1, nums2, r)) / 2
+	return (float64)(getKth(nums1, nums2, l)+getKth(nums1, nums2, r)) / 2
 }
--- src/p004/p004_test.go
+++ src/p004/p004_test.go
@@ -1,23 +1,23 @@
 package p004
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFindMedianSortedArrays(t *testing.T) {
-	nums11 := []int {1,3}
-	nums12 := []int {2}
-	med1   := 2.0
-	res1   := findMedianSortedArrays(nums11, nums12)
-
-	if (med1 != res1) {
-		t.Errorf("Expected %v, got %v", med1, res1)
+	var tests = []struct {
+		in0, in1 []int
+		want     float64
+	}{
+		{[]int{1, 3}, []int{2}, 2.0},
+		{[]int{1, 2}, []int{3, 4}, 2.5},
 	}
 
-	nums21 := []int {1,2}
-	nums22 := []int {3,4}
-	med2   := (2+3)/2.0
-	res2   := findMedianSortedArrays(nums21, nums22)
-
-	if (med2 != res2) {
-		t.Errorf("Expected %v, got %v", med2, res2)
+	for _, test := range tests {
+		got := findMedianSortedArrays(test.in0, test.in1)
+		if got != test.want {
+			t.Errorf("findMedianSortedArrays(%#v, %#v) = %v, want %v",
+				test.in0, test.in1, got, test.want)
+		}
 	}
 }
