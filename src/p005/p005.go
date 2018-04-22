27e4a85f0d10f9e965969b552ffab06f7690220b
Lee Duhem <lee.duhem@gmail.com>
2018-04-23 00:49:11 +0800

    Revise Solution to Problem #5

--- src/p005/p005.go
+++ src/p005/p005.go
@@ -1,33 +1,29 @@
+// https://leetcode.com/problems/longest-palindromic-substring/description/
 package p005
 
-func isPalindrome(s string) bool {
-	l := len(s)
-	h := l / 2
-
-	for i,v := range s[0:h] {
-		//if (v != []rune(s)[l-1-i]) {
-		// is more proper, but leetcode thought it's too slow.
-		if (byte(v) != s[l-1-i]) {
+func isPalindrome(s []rune) bool {
+	n := len(s) / 2
+	for i := 0; i < n; i++ {
+		if s[i] != s[len(s)-1-i] {
 			return false
 		}
 	}
-
 	return true
 }
 
 func longestPalindrome(s string) string {
-	l   := len(s)
-	low,high := 0,0
-	max := 0
+	rs := []rune(s)
+	l := len(rs)
+	low, high, max := 0, 0, 0
 
-	for i,_ := range s {
-		for j := i+1; j <= l; j++ {
-			if (isPalindrome(s[i:j]) && (j-i+1 > max)) {
-				max = j-i+1
-				low,high = i,j
+	for i := range rs {
+		for j := i + 1; j <= l; j++ {
+			if isPalindrome(rs[i:j]) && (j-i+1 > max) {
+				max = j - i + 1
+				low, high = i, j
 			}
 		}
 	}
 
-	return s[low:high]
+	return string(rs[low:high])
 }
