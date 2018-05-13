a41e8fdd889d50c23661b58ec7d839bbd83d710e
Lee Duhem <lee.duhem@gmail.com>
2018-05-13 22:02:38 +0800

    Revise Solution to Problem #10

--- src/p010/p010.go
+++ src/p010/p010.go
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/regular-expression-matching/description/
 package p010
 
 // NB. Real regexp engine is far more complex than this,
@@ -7,13 +8,13 @@ func isMatch(s string, p string) bool {
 	strlen, patlen := len(s), len(p)
 	strpos, patpos := 0, 0
 
-	for ; strpos < strlen && patpos < patlen; {
+	for strpos < strlen && patpos < patlen {
 		zeroMore := false
-		if (patpos < patlen - 1 && p[patpos+1] == '*') {
+		if patpos < patlen-1 && p[patpos+1] == '*' {
 			zeroMore = true
 		}
 
-		if (p[patpos] == '.' || p[patpos] == s[strpos]) {
+		if p[patpos] == '.' || p[patpos] == s[strpos] {
 			strpos++
 			if !zeroMore {
 				patpos++
@@ -26,42 +27,42 @@ func isMatch(s string, p string) bool {
 	}
 
 	if strpos == 0 {
-		if strlen == 0 {
-			if patlen == 0 {
-				return true
-			}
-
-			// Ignore further ZeroOrMore patterns
-			for ; patpos < patlen - 1 && p[patpos+1] == '*'; patpos += 2 {
-			}
-			if patpos == patlen {
-				return true
-			}
-
+		if strlen != 0 {
 			return false
 		}
+
+		if patlen == 0 {
+			return true
+		}
+
+		// Ignore further ZeroOrMore patterns
+		for ; patpos < patlen-1 && p[patpos+1] == '*'; patpos += 2 {
+		}
+		if patpos == patlen {
+			return true
+		}
+
 		return false
 	}
 
 	if strpos == strlen {
 		// Ignore further ZeroOrMore patterns
-		for ; patpos < patlen - 1 && p[patpos+1] == '*'; patpos += 2 {
+		for ; patpos < patlen-1 && p[patpos+1] == '*'; patpos += 2 {
 		}
 		if patpos == patlen {
 			return true
 		}
 	}
 
-
 	// backtrack
 	if 0 < patpos && patpos < patlen {
-		for strpos1 := strlen-1; strpos1 >= 0; strpos1-- {
+		for strpos1 := strlen - 1; strpos1 >= 0; strpos1-- {
 			if isMatch(s[0:strpos1], p[0:patpos]) && isMatch(s[strpos1:], p[patpos:]) {
 				return true
 			}
 		}
 
-		for patpos1 := patpos-2; patpos1 >= 0; {
+		for patpos1 := patpos - 2; patpos1 >= 0; {
 			if isMatch(s[0:strpos], p[0:patpos1]) && isMatch(s[strpos:], p[patpos1:]) {
 				return true
 			}
--- src/p010/p010_test.go
+++ src/p010/p010_test.go
@@ -1,30 +1,38 @@
 package p010
 
-import "testing"
-
-func testCase(s, p string, r0 bool, t *testing.T) {
-	r := isMatch(s, p)
-	if (r != r0) {
-		t.Errorf("Case %v %v: expected %v, got %v", s, p, r0, r)
-	}
-}
+import (
+	"testing"
+)
 
 func TestIsMatch(t *testing.T) {
-	testCase("aa", "a", false, t)
-	testCase("aa", "aa", true, t)
-	testCase("aaa", "aa", false, t)
-	testCase("aa", "a*", true, t)
-	testCase("aa", ".*", true, t)
-	testCase("ab", ".*", true, t)
-	testCase("aab", "c*a*b", true, t)
-	testCase("ab", ".*c", false, t)
-	testCase("aaa", "a*a", true, t)
-	testCase("a", "ab*", true, t)
-	testCase("abbbcd", "ab*bbbcd", true, t)
-	testCase("bbbacbaacacaaaba", "b*c*b*.a.*a*.*.*b*", true, t)
-	testCase("aaba", "ab*a*c*a", false, t)
-	testCase("ab", ".*..", true, t)
-	testCase("a", "ab*a", false, t)
-	testCase("", "c*", true, t)
-	testCase("", "c*c*", true, t)
+	tests := []struct {
+		in0, in1 string
+		want     bool
+	}{
+		{"aa", "a", false},
+		{"aa", "aa", true},
+		{"aaa", "aa", false},
+		{"aa", "a*", true},
+		{"aa", ".*", true},
+		{"ab", ".*", true},
+		{"aab", "c*a*b", true},
+		{"ab", ".*c", false},
+		{"aaa", "a*a", true},
+		{"a", "ab*", true},
+		{"abbbcd", "ab*bbbcd", true},
+		{"bbbacbaacacaaaba", "b*c*b*.a.*a*.*.*b*", true},
+		{"aaba", "ab*a*c*a", false},
+		{"ab", ".*..", true},
+		{"a", "ab*a", false},
+		{"", "c*", true},
+		{"", "c*c*", true},
+	}
+
+	for _, test := range tests {
+		got := isMatch(test.in0, test.in1)
+		if got != test.want {
+			t.Errorf("isMatch(%q, %q) = %v, want %v",
+				test.in0, test.in1, got, test.want)
+		}
+	}
 }
